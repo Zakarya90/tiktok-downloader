c562bfe5f6afe61fcc79e21037a37f1482437e87
Zakarya <133411745+Zakarya90@users.noreply.github.com>
2023-08-31 23:11:54 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+// pages/index.js
+
 import React, { useState } from 'react';
 import axios from 'axios';
+import VideoDownloader from '../components/VideoDownloader';
 
 export default function Home() {
   const [videoUrl, setVideoUrl] = useState('');
@@ -17,22 +20,24 @@ export default function Home() {
     }
   };
 
- return (
-  <div>
-    <h1>TikTok Video Downloader</h1>
-    <input
-      type="text"
-      placeholder="Enter TikTok video URL"
-      value={videoUrl}
-      onChange={(e) => setVideoUrl(e.target.value)}
-    />
-    <button onClick={handleDownload}>Download</button>
-    {downloadLink && (
-      <div>
-        <a href={downloadLink} download="tiktok_video.mp4">
-          Click here to download the video
-        </a>
-      </div>
-    )}
-  </div>
-);
+  return (
+    <div>
+      <h1>TikTok Video Downloader</h1>
+      <input
+        type="text"
+        placeholder="Enter TikTok video URL"
+        value={videoUrl}
+        onChange={(e) => setVideoUrl(e.target.value)}
+      />
+      <button onClick={handleDownload}>Download</button>
+      {downloadLink && (
+        <div>
+          <a href={downloadLink} download="tiktok_video.mp4">
+            Click here to download the video
+          </a>
+        </div>
+      )}
+      <VideoDownloader />
+    </div>
+  );
+}
