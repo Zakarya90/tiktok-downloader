63973d9bfa4fe308500c1d298ccd8dcaa110df68
Zakarya <133411745+Zakarya90@users.noreply.github.com>
2023-08-31 20:55:19 +0100

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -7,7 +7,7 @@ export default {
     locale: 'en_IE',
     url: 'https://your-website-url.com', // Replace with your actual URL
     site_name: 'TikTok Downloader', // Replace with your site name
-    // Add other openGraph settings as needed
+    // Add other openGraph settings as needed You can add more SEO configuration settings like Twitter cards, additional meta tags, etc
   },
   // Add other SEO configuration settings as needed
 };
