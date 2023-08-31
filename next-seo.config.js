b40c2a3bf85b92f75ff1cd3e7a3124d7b5c5f5a6
Zakarya <133411745+Zakarya90@users.noreply.github.com>
2023-08-31 20:21:45 +0100

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,1 +1,14 @@
+// next-seo.config.js
+export default {
+  title: 'TikTok Downloader', // Default title for your site
+  description: 'Download TikTok videos easily.', // Default description for your site
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://your-website-url.com', // Replace with your actual URL
+    site_name: 'TikTok Downloader', // Replace with your site name
+    // Add other openGraph settings as needed
+  },
+  // Add other SEO configuration settings as needed
+};
 
