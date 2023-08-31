2dabf9004c97e4f8827cee94dbb7e1884992be71
Zakarya <133411745+Zakarya90@users.noreply.github.com>
2023-08-31 18:55:12 +0100

    Update download.js

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const response = await axios.get(req.body.url, { responseType: 'blob' });
+      res.setHeader('Content-Type', 'video/mp4');
+      res.send(response.data);
+    } catch (error) {
+      console.error('Error downloading video:', error);
+      res.status(500).send('Error downloading video');
+    }
+  } else {
+    res.status(405).send('Method not allowed');
+  }
+}
 
