92b396d209c7fc7c25c5d59803b251be05b319eb
Zakarya <133411745+Zakarya90@users.noreply.github.com>
2023-08-31 19:13:33 +0100

    Update download.js

--- pages/api/download.js
+++ pages/api/download.js
@@ -4,8 +4,11 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const response = await axios.get(req.body.url, { responseType: 'blob' });
+
+      // Create a Blob from the video data and send it as a response
+      const blob = new Blob([response.data], { type: 'video/mp4' });
       res.setHeader('Content-Type', 'video/mp4');
-      res.send(response.data);
+      res.send(blob);
     } catch (error) {
       console.error('Error downloading video:', error);
       res.status(500).send('Error downloading video');
